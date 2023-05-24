ba907ba625a76991285e903d12cac4ad7c0bb8a3
Muhammad Zubair Irshad <muhammadzubairirshad@gmail.com>
2023-05-24 17:49:33 -0400

    train on PD

--- srt/data/__init__.py
+++ srt/data/__init__.py
@@ -2,5 +2,6 @@ from srt.data.core import get_dataset, worker_init_fn
 from srt.data.nmr import NMRDataset
 from srt.data.multishapenet import MultishapenetDataset 
 from srt.data.obsurf import Clevr3dDataset
+from srt.data.pdmultiobject_ae_nocs import PDMultiObject_AE_NOCS
 # from srt.data.objectron import ObjectronMultiDataset
 
