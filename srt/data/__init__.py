e6fe8bc378260329496ab335a9e3d362e7a0ff72
Muhammad Zubair Irshad <muhammadzubairirshad@gmail.com>
2023-05-24 16:17:31 -0400

    train on NMR

--- srt/data/__init__.py
+++ srt/data/__init__.py
@@ -2,5 +2,5 @@ from srt.data.core import get_dataset, worker_init_fn
 from srt.data.nmr import NMRDataset
 from srt.data.multishapenet import MultishapenetDataset 
 from srt.data.obsurf import Clevr3dDataset
-from srt.data.objectron import ObjectronMultiDataset
+# from srt.data.objectron import ObjectronMultiDataset
 
